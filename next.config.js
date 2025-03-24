cd1eb5ab1f738d38d7f176720205ec13d38db270
varun <varunpeela01@gmail.com>
2025-03-24 14:54:45 +0530

    Fix postbuild script to copy routes-manifest.json to out/ using xcopy

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   trailingSlash: true,
   basePath: "",
   reactStrictMode: true,
-  
+  distDir: "out",
 };
-
 module.exports = nextConfig;
